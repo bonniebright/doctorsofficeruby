74c6318ce425b7ceb26c995be11bf8e990e3d404
bailey & Jason Hamilton <student@epicodus.com>
2014-03-06 16:56:36 -0800

    Working Calc with split on question mark

--- Calculator/lib/calculator.rb
+++ Calculator/lib/calculator.rb
@@ -1,18 +1,37 @@
 def calculator(sentence)
   math = {"plus" => :+, "minus" => :-, "multiplied" => :*, "divided" => :/, "power" => :**}
-  sentences = sentence.split("?")
-  answer = []
-  sentences.each do |sentence|
-    words = sentence.split
-    equations = []
-    words.each do |word|
-      if math.include? word
-        equations << math[word]
-      elsif /[0-9]/.match word 
-        equations << word.to_f 
-      end
+  words = sentence.split
+  equations = []
+
+  words.each do |word|
+    if math.include? word
+      equations << math[word]
+    elsif /[0-9]/.match word 
+      equations << word.to_f 
     end
-    answer.push(eval equations.join)  
   end
-  answer.join(", ")     
+
+  answer = eval equations.join
+  result = ""
+
+  if answer == answer.to_i
+    result = answer.to_i
+  else
+    result = answer
+  end
+  result     
+end
+
+
+def sentences_calc(sentences)
+  sentence_array = sentences.split("?")
+  calcs = []
+  if sentence_array.length == 1
+    calculator(sentence[0])
+  else
+    sentence_array.each do |sent|
+      calcs << calculator(sent)
+    end
+    calcs
+  end
 end
